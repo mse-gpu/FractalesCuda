70bf005b3e2b46469d9d125b63998e19b5c403e8
Baptiste Wicht <baptiste.wicht@gmail.com>
2012-03-22 20:29:55 +0100

    First commit

---
+++ cudaLinux.mk
@@ -0,0 +1,8 @@
+EXCLUDE_LIBRARY_FILES:=
+ADD_LIBRARY_FILES:=
+CXXFLAGS   = 
+CXXLDFLAGS = 
+COMPILATEUR:=nvidia
+OS:=Linux
+-include dataProject.mk
+-include ../PRODUCTION/MANAGER/makefile/private/makeFileCudaLinux.mk
---
+++ dataProject.mk
@@ -0,0 +1,8 @@
+TARGET_NAME:=FractalesCuda
+TARGET_MODE:=EXE
+TARGET_PATH:=/tmp/ramdrive/${USER}/Targets/${TARGET_NAME}
+TARGET_DEPLOY_PATH:=#../PRODUCTION/Deploy/bin
+CODE_DEFINE_VARIABLES+=
+SRC_AUX:=  ../PRODUCTION/commonExt/cuda/  ../PRODUCTION/commonExt/include/
+NVCCFLAGS= -O3 -use_fast_math -arch=sm_20
+NVCCLDFLAGS =
