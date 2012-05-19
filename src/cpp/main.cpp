3c6971457893a0bebdb7edc058e6c435095b2e4c
Baptiste Wicht <baptiste.wicht@gmail.com>
2012-05-19 14:11:29 +0200

    Finalize benchmark

--- src/cpp/main.cpp
+++ src/cpp/main.cpp
@@ -71,83 +71,91 @@ int launchApplication(int argc, char** argv){
     }
 }
 
-#define DIM_H 12000
-#define DIM_W 16000
+#define DIM_H 2000
+#define DIM_W 2000
+#define TIMES 20
 
 void launchMandelBrotAnimation(uchar4* ptrDevPixels, int w, int h, int N, const DomaineMaths& domainNew);
 void launchJuliaAnimation(uchar4* ptrDevPixels, int w, int h, int N, const DomaineMaths& domainNew);
+
+void bench(int N, uchar4* image){
+    std::cout << "Start Benchmark with N=" << N << std::endl;
+
+    CUevent start;
+    CUevent stop;
+    HANDLE_ERROR(cudaEventCreate(&start, CU_EVENT_DEFAULT));
+    HANDLE_ERROR(cudaEventCreate(&stop, CU_EVENT_DEFAULT));
+    HANDLE_ERROR(cudaEventRecord(start,0));
+
+    float xMin = -1.3968;
+    float xMax = -1.3578;
+    float yMin = -0.03362;
+    float yMax = 0.0013973;
+
+    DomaineMaths domain1(xMin, yMin, xMax - xMin, yMax - yMin);
+
+    for(int i = 0; i < TIMES; ++i){
+	launchMandelBrotAnimation(image, DIM_W, DIM_H, N, domain1);
+    }
+
+    float elapsed = 0;
+    HANDLE_ERROR(cudaEventRecord(stop,0));
+    HANDLE_ERROR(cudaEventSynchronize(stop));
+    HANDLE_ERROR(cudaEventElapsedTime(&elapsed, start, stop));
+
+    std::cout << "MandelBrot Total (" << TIMES << " times) = " << elapsed << "ms" << std::endl;
+    std::cout << "MandelBrot Mean  (" << TIMES << " times) = " << (elapsed / TIMES) << "ms" << std::endl;
+
+    HANDLE_ERROR(cudaEventRecord(start,0));
+
+    xMin = -1.3968;
+    xMax = -1.3578;
+    yMin = -0.03362;
+    yMax = 0.0013973;
+
+    DomaineMaths domain2(xMin, yMin, xMax - xMin, yMax - yMin);
+
+    for(int i = 0; i < TIMES; ++i){
+	launchJuliaAnimation(image, DIM_W, DIM_H, N, domain2);
+    }
+
+    elapsed = 0;
+    HANDLE_ERROR(cudaEventRecord(stop,0));
+    HANDLE_ERROR(cudaEventSynchronize(stop));
+    HANDLE_ERROR(cudaEventElapsedTime(&elapsed, start, stop));
+
+    std::cout << "Julia Total (" << TIMES << " times) = " << elapsed << "ms" << std::endl;
+    std::cout << "Julia Mean  (" << TIMES << " times) = " << (elapsed / TIMES) << "ms" << std::endl;
+
+    HANDLE_ERROR(cudaEventDestroy(start));
+    HANDLE_ERROR(cudaEventDestroy(stop));
+}
 
 int bench(int argc, char** argv){
     std::cout << "Launch benchmark" << std::endl;
 
     if (nbDeviceDetect() >= 1){
-	    int deviceId = 1;
+	int deviceId = 1;
 
-	    HANDLE_ERROR(cudaSetDevice(deviceId)); // active gpu of deviceId
-	    HANDLE_ERROR(cudaSetDeviceFlags(cudaDeviceMapHost)); // Not all gpu allow the use of mapMemory (avant prremier appel au kernel)
+	HANDLE_ERROR(cudaSetDevice(deviceId)); // active gpu of deviceId
+	HANDLE_ERROR(cudaSetDeviceFlags(cudaDeviceMapHost)); // Not all gpu allow the use of mapMemory (avant prremier appel au kernel)
 
-	    //Force the driver to run
-	    int* fake;
-	    HANDLE_ERROR(cudaMalloc((void**) &fake, sizeof(int)));
+	//Force the driver to run
+	uchar4* image;
+	HANDLE_ERROR(cudaMalloc(&image, DIM_W * DIM_H * sizeof(uchar4)));
 
-	    uchar4* image;
-	    HANDLE_ERROR(cudaMalloc(&image, DIM_W * DIM_H * sizeof(uchar4)));
+	std::cout << "End of malloc" << std::endl;
+	std::cout << "Size of the image: " << (DIM_W * DIM_H * sizeof(uchar4)) << std::endl;
 
-	    std::cout << "End of malloc" << std::endl;
-	    std::cout << "Size of the image: " << (DIM_W * DIM_H * sizeof(uchar4)) << std::endl;
+	bench(10, image);
+	bench(25, image);
+	bench(50, image);
+	bench(100, image);
 
-	    CUevent start;
-	    CUevent stop;
-	    HANDLE_ERROR(cudaEventCreate(&start, CU_EVENT_DEFAULT));
-	    HANDLE_ERROR(cudaEventCreate(&stop, CU_EVENT_DEFAULT));
-	    HANDLE_ERROR(cudaEventRecord(start,0));
+	HANDLE_ERROR(cudaFree(image));
 
-	    float xMin = -1.3968;
-	    float xMax = -1.3578;
-	    float yMin = -0.03362;
-	    float yMax = 0.0013973;
-
-	    DomaineMaths domain1(xMin, yMin, xMax - xMin, yMax - yMin);
-
-	    for(int i = 0; i < 10; ++i){
-		launchMandelBrotAnimation(image, DIM_W, DIM_H, 100, domain1);
-	    }
-
-	    float elapsed = 0;
-	    HANDLE_ERROR(cudaEventRecord(stop,0));
-	    HANDLE_ERROR(cudaEventSynchronize(stop));
-	    HANDLE_ERROR(cudaEventElapsedTime(&elapsed, start, stop));
-
-	    std::cout << "MandelBrot CUDA Version took " << elapsed << "ms" << std::endl;
-
-	    HANDLE_ERROR(cudaEventRecord(start,0));
-
-	    xMin = -1.3968;
-	    xMax = -1.3578;
-	    yMin = -0.03362;
-	    yMax = 0.0013973;
-
-	    DomaineMaths domain2(xMin, yMin, xMax - xMin, yMax - yMin);
-
-	    for(int i = 0; i < 10; ++i){
-		launchJuliaAnimation(image, DIM_W, DIM_H, 100, domain2);
-	    }
-
-	    elapsed = 0;
-	    HANDLE_ERROR(cudaEventRecord(stop,0));
-	    HANDLE_ERROR(cudaEventSynchronize(stop));
-	    HANDLE_ERROR(cudaEventElapsedTime(&elapsed, start, stop));
-
-	    std::cout << "Julia CUDA Version took " << elapsed << "ms" << std::endl;
-
-	    HANDLE_ERROR(cudaEventDestroy(start));
-	    HANDLE_ERROR(cudaEventDestroy(stop));
-
-	    HANDLE_ERROR(cudaFree(image));
-	    HANDLE_ERROR(cudaFree(fake));
-
-	    return EXIT_SUCCESS;
+	return EXIT_SUCCESS;
     } else {
-	    return EXIT_FAILURE;
+	return EXIT_FAILURE;
     }
 }
